0821d059ef9a5eee500eb17396f4151ebe9181c6
r1nzl3r-bl4ck <r1nzl3r-bl4ck@protonmail.com>
2023-10-24 09:54:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def api_ad_stats(ad_id):
         'num_views' : views,
         'num_contacts' : contacts,
         'emoji' : emoji_calculator(views),
-        'version' : '1.0'
+        'version' : '2.0'
     }
 
     resp = jsonify(ad_data)
